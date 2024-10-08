b4528dcc7ea35ddb2f1e8c2f39aaff0eea44c8a7
Motko <77466238+Motko222@users.noreply.github.com>
2024-10-08 12:23:55 +0200

    Create report.sh

---
+++ report.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd)
+folder=$(echo $path | awk -F/ '{print $NF}')
+json=~/logs/report-$folder
+source ~/.bash_profile
+
+chain=?
+network=?
+version=?
+container=$(docker ps | grep $folder | awk '{print $NF}')
+[ $container ] && docker_status=$(docker inspect $container | jq -r .[].State.Status)
+
+case $docker_status in
+  running) status=ok; message="..." ;;
+  *) status="warning"; message="..." ;;
+esac
+
+cat >$json << EOF
+{
+  "updated":"$(date --utc +%FT%TZ)",
+  "measurement":"report",
+  "tags": {
+   "id":"$folder",
+   "machine":"$MACHINE",
+   "grp":"node",
+   "owner":"$OWNER"
+  },
+  "fields": {
+   "network":"$network",
+   "chain":"$chain",
+   "status":"$status",
+   "message":"$message",
+   "docker_status":"$docker_status",
+   "version":"$version"
+  }
+}
+EOF
+
+cat $json
